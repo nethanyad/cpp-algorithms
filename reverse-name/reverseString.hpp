9821e233b4e23ef5297be43b239e07badb1ee4af
Nethanya Dhaiphule <151404282+nethanyad@users.noreply.github.com>
2025-08-13 10:48:07 +0530

    Update reverseString.hpp

--- reverse-name/reverseString.hpp
+++ reverse-name/reverseString.hpp
@@ -1,3 +1,13 @@
+/*
+---------------------------------------------------------
+Project:    Reverse Name
+Author:     Nethanya Dhaiphule
+Language:   C++
+Description:
+      Reads a string from the user and outputs its reverse
+      using a stack data structure.
+---------------------------------------------------------
+*/
 #include <iostream>
 #include "stack.hpp"
 #include <sstream>
@@ -39,3 +49,4 @@ public:
         }
     }
 };
+
